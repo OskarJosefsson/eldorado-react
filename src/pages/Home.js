338b364c8ed91fe890cdb8c198567e981c658f9e
OskarJosefsson <OskarJosefsson12@Gmail.com>
2022-05-04 10:15:54 +0200

    structure comments

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,19 @@
 import React from "react";
 const Home = () => {
-  return <React.Fragment></React.Fragment>;
+  return <React.Fragment>
+    <section>
+    </section>
+    {/* Navbar
+    Banner */}
+    <section>
+     {/* Component 1
+     Component 2
+     Component 3*/}
+    </section>
+    <section>
+      {/* Component 1
+      Component 2 */}
+    </section>
+  </React.Fragment>;
 };
 export default Home;
