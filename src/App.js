11fc3d68b145f9cb800f51740837f7b1dafa6745
OskarJosefsson <OskarJosefsson12@Gmail.com>
2022-05-04 08:38:02 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import { Routes, Route } from "react-router-dom";
 import Wishlist from "./pages/Wishlist";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
+import Navbar from "./components/navbar/navbar";
+import Shoppingcart from "./pages/Shoppingcart";
+import Shop from "./pages/Shop";
 
 function App() {
   return (
     <div>
-      {" "}
+      <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="Shop" element={<Shop />} />
         <Route path="Wishlist" element={<Wishlist />} />
         <Route path="Products" element={<Products />} />
+        <Route path="Shoppingcart" element={<Shoppingcart />} />
       </Routes>
     </div>
   );
