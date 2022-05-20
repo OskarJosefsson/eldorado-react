fafdfbf0a44a70241a7a182723ac175c61ba9dd5
TomRingblom <t_ringblom@hotmail.com>
2022-05-20 12:10:55 +0200

    Added info about the user on /Account page from Azure AD B2C claims

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,15 +1,25 @@
-import { Fragment } from "react";
-import { useMsal } from "@azure/msal-react";
+import { useAccount, useMsal } from "@azure/msal-react";
+import { useEffect, useState } from "react";
 import { b2cPolicies } from "../authConfig";
 
+import classes from "./Account.module.css";
+
 const Account = () => {
-  const { instance } = useMsal();
+  const { instance, accounts } = useMsal();
+  const { name, username } = accounts[0];
+  const { streetAddress, postalCode, city} = accounts[0].idTokenClaims;
+  const [ user, setUser ] = useState({});
 
   return (
-  <Fragment>
-    <button onClick={() => instance.logoutPopup({postLogoutRedirectUri: "/",mainWindowRedirectUri: "/",})}>Logout</button>
-    <button onClick={() => instance.loginPopup(b2cPolicies.authorities.editProfile)}>Edit profile</button>
-  </Fragment>
+    <div className={classes.info}>
+        <strong>{user.name}</strong>
+        <p><strong>Email:</strong> {username}</p>
+        <p><strong>Street:</strong> {streetAddress}</p>
+        <p><strong>Postal Code:</strong> {postalCode}</p>
+        <p><strong>City:</strong> {city}</p>
+        <button className={classes.btn} onClick={() => instance.logoutPopup({postLogoutRedirectUri: "/",mainWindowRedirectUri: "/",})}>Logout</button>
+        <button className={classes.btn} onClick={() => instance.loginPopup(b2cPolicies.authorities.editProfile)}>Edit profile</button>
+    </div>
   );
 };
 
